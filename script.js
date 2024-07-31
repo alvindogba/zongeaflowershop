b21439908994f0eefb699967ff496658d40af15f
Titus Cheabow <tituscheabow4@gmail.com>
2024-07-31 06:57:18 -0700

    titus update

---
+++ script.js
@@ -0,0 +1,89 @@
+
+
+ document.addEventListener('DOMContentLoaded', (e)=>{
+    let firstCard = document.getElementById("firstCard");
+    let bagLove = document.getElementById("bag-love-s");
+    firstCard.addEventListener('mouseenter', (e) => {
+        bagLove.style.display = "flex";   
+    
+    });
+    firstCard.addEventListener('mouseleave', (e) => {
+        bagLove.style.display = "none";   
+    });
+   });
+
+ document.addEventListener('DOMContentLoaded', ()=>{
+    let secondCard = document.getElementById("secondCard");
+    let bagLove = document.getElementById("bagLove");
+    secondCard.addEventListener('mouseenter', (e) => {
+        bagLove.style.display = "flex";   
+    });
+    secondCard.addEventListener('mouseleave', (e) => {
+        bagLove.style.display = "none";   
+    });
+ });
+ document.addEventListener('DOMContentLoaded', ()=>{
+    let thirdCard = document.getElementById("thirdCard");
+    let bagLLove = document.getElementById("bagLLove");
+    thirdCard.addEventListener('mouseenter', (e) => {
+        bagLLove.style.display = "flex";   
+    });
+    thirdCard.addEventListener('mouseleave', (e) => {
+        bagLLove.style.display = "none";   
+    });
+ });
+
+  document.addEventListener('DOMContentLoaded',() =>{
+    let fourthCard = document.getElementById("fourthCard");
+    let bagLLLove = document.getElementById("bagLLLove");
+    fourthCard.addEventListener('mouseenter',(e)=>{
+        bagLLLove.style.display = "flex";
+    })
+    fourthCard.addEventListener('mouseleave',(e)=>{
+        bagLLLove.style.display = "none";
+    })
+  })
+
+  document.addEventListener('DOMContentLoaded',() =>{
+    let fifthCard = document.getElementById("fifthCard");
+    let one = document.getElementById("one");
+    fifthCard.addEventListener('mouseenter',(e)=>{
+        one.style.display = "flex";
+    })
+     fifthCard.addEventListener('mouseleave',(e)=>{
+        one.style.display = "none";
+    })
+  })
+  
+  document.addEventListener('DOMContentLoaded',() =>{
+    let sixthCard = document.getElementById("sixthCard");
+    let two = document.getElementById("two");
+    sixthCard.addEventListener('mouseenter',(e)=>{
+        two.style.display = "flex";
+    })
+     sixthCard.addEventListener('mouseleave',(e)=>{
+        two.style.display = "none";
+    })
+  })
+
+  document.addEventListener('DOMContentLoaded',() =>{
+    let seventhCard = document.getElementById("seventhCard");
+    let three = document.getElementById("three");
+    seventhCard.addEventListener('mouseenter',(e)=>{
+        three.style.display = "flex";
+    })
+     seventhCard.addEventListener('mouseleave',(e)=>{
+        three.style.display = "none";
+    })
+  })
+  document.addEventListener('DOMContentLoaded',() =>{
+    let eightCard = document.getElementById("eightCard");
+    let fourth = document.getElementById("fourth");
+    eightCard.addEventListener('mouseenter',(e)=>{
+        fourth.style.display = "flex";
+    })
+     eightCard.addEventListener('mouseleave',(e)=>{
+        fourth.style.display = "none";
+    })
+  })
+  
